5723e383bdf7dcc26f165e45bef8d1e357ae1fdc
lee23michael <lee23michael@gmail.com>
2021-11-21 14:12:00 -0800

    Add files via upload

--- Lab7/olympic_rings.py
+++ Lab7/olympic_rings.py
@@ -3,9 +3,16 @@ import draw_USA_flag
 import drawCircle
 import print_me_first
 
+"""
+This program will draw a USA flag on the screen and an USA map as backgroun
+A tiltl "USA OLYMPIC TEAM" will be drew on top of the flag, and a Olympic ring
+will be drew below the flag. Also, Auther's name, program name, and current time
+will be printed on the right bottom corner 
+"""
+
 def main():
 
-    # create a screen
+#------ create a screen------
     screen = turtle.getscreen()
         # set background color of screen
     screen.bgcolor("white")
@@ -13,29 +20,23 @@ def main():
     screen.title("USA Flag - https://www.pythoncircle.com")
         # set usa map as background
     turtle.bgpic("usa.png")
-        # Write "USA OLYMPIC TEAM" on top 
+
+#------drawing------
+        # write "USA OLYMPIC TEAM" on top 
     turtle.hideturtle()
     turtle.penup()
     turtle.goto(0,300)
     turtle.pendown()
     turtle.pencolor("blue")
     turtle.write("USA OLYMPIC TEAM", align = 'center', font = (" ",40,'bold'))
-        # draw usa flag
+
+        # draw USA flag
     draw_USA_flag.draw_USA_flag()
-
-        
-
-
-
-        # a list of color of olympic rings
-    color = ['blue','orange','black','green','red']
-
+    color = ['blue','orange','black','green','red']# a list of color of olympic rings
     count = 0   # a counter variable
     x = -180    # starting x value of the first ring
     y = -300    # statting y value of the first ring
-
-        # A loop of drawing 5 rings
-    for count in range(5) :
+    for count in range(5):   # A loop of drawing 5 rings
         drawCircle.drawCircle(x,y,color[count])
         x += 87.5
         if y == -300:
@@ -44,15 +45,14 @@ def main():
             y = -300
         count +=1
 
-        # print authoer info on screen
+        # print authoer's info on screen
     info = print_me_first.print_first("Ziwen Li")
     turtle.penup()
     turtle.goto(300,-350)
     turtle.pencolor('blue')
     turtle.write(info)
-
-
-    screen.mainloop()
+ 
+    screen.mainloop()   # keep holding the screen until closed manually
 
 if __name__ == "__main__":
     main()
