2c3d4f103c7e60eb6d699296eeb989fb401d3b13
lee23michael <lee23michael@gmail.com>
2021-11-21 13:56:59 -0800

    Add files via upload

---
+++ Lab7/print_me_first.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import sys
+import os
+"""
+This program will print out the basic information of the author 
+
+@name programmer's name
+@labNum  lab number
+@labName lab's name
+"""
+def print_first(name):
+
+# name = "CNET-142: Ziwen li"
+# labName = "Lab 4: Functions"
+    time = datetime.now()
+    currenTime = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    fileName = os.path.basename(sys.argv[0])
+
+    msg = "Name\t\t: "+name + '\nprogram\t\t: '+fileName+\
+    "\nCurrent Time\t: " + str(currenTime)[:19]
+ 
+    return(msg)
